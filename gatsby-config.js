cc97d5949f4ff2c12a15e373d00e66cf9f65a2bd
Deepak Savani <deepak.savani@brainvire.com>
2021-10-04 13:51:09 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "First-Project",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
